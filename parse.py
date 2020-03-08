82f70c63f8ddc1e9991f0dbc58f5e6e620a9c29d
sb <sbenthall@gmail.com>
2020-03-08 14:43:29 -0400

    tweaking parser/regex to capture more data variation

--- parse.py
+++ parse.py
@@ -4,19 +4,17 @@ import os
 import re
 from search import filename_from_rfc_number, iterate_rfc_files, normalize_rfc_number, JSON_INPUT_FILENAME
 
-author_rexp = "   (\w\. \w+,? ?\w+\.?)$"
-affiliation_rexp = "   (\w[\w \.]+)$"
-date_rexp = "   (\w+ \d\d\d\d)$"
+author_rexp = "   (\w\. [\w-]+,? ?\w+\.?)$"
+affiliation_rexp = "   (\w[\w \.,\+-]+)$"
+date_rexp = "   ((?:\d?(\d )?)?\w+ \d\d\d\d)$"
 
 
-def extract_metadata(rfc_filename):
+def extract_metadata(filename):
     """
     Returns a list of author, affiliation, and date
     dicts in order that they appear in the RFC text.
     """
-
-    filename = filename_from_rfc_number(rfc_filename[:-4])
-
+    
     with open(filename, 'r') as txt_file:
         lines = txt_file.readlines()
 
@@ -38,6 +36,8 @@ def extract_metadata(rfc_filename):
                 metadata.append({'date' : date_match[1]})
             elif affiliation_match:
                 metadata.append({'affiliation' : affiliation_match[1]})
+            elif not line == "":
+                pass
             elif not header:
                 return metadata
 
@@ -74,9 +74,15 @@ def main():
     filenames = os.listdir('RFC-all')
    
     for filename in filenames:
-        metadata = extract_metadata(filename)
-        cm = compile_metadata(metadata)
-        print(cm)
+        fn = filename_from_rfc_number(filename[:-4])
+        if fn is not None:
+            metadata = extract_metadata(fn)
+            cm = compile_metadata(metadata)
+
+            if cm is not None:
+                print(cm)
+            else:
+                print(filename + " metadata was not parsed.")
         
 if __name__== "__main__":
     main()
